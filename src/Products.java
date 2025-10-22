52234ff158a62d91efd45f0d656fb2d17cdf6eec
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-22 22:25:46 +0530

    Commit1

---
+++ src/Products.java
@@ -0,0 +1,28 @@
+public class Products {
+    private int id;
+    private String name;
+    private int quantity;
+    private int reorderThreshold;
+    public Product(int id, String name, int quantity, int reorderThreshold) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.reorderThreshold = reorderThreshold;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getReorderThreshold() {
+        return reorderThreshold;
+    }
+}
