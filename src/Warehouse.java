baf03d273952553c4905103d86f7c4a620eeeb1a
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-25 13:48:21 +0530

    Added Warehouse class with inventory operations

---
+++ src/Warehouse.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Warehouse {
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+    }
+}
