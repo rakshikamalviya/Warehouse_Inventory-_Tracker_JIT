b8c8a313f3660e5418d3366b2c1ee7a2a5db0e8a
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-30 18:53:59 +0530

    Fixed Inventory class Errors

--- src/Inventory.java
+++ src/Inventory.java
@@ -2,65 +2,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Inventory {
-    private Map<String, Warehouse> warehouses;
+    private Map<Integer, Products> productsList = new HashMap<>();
 
-    public Inventory() {
-        warehouses = new HashMap<>();
-    }
-
-    public void addWarehouse(String name) {
-        if (!warehouses.containsKey(name)) {
-            warehouses.put(name, new Warehouse(name));
-            System.out.println("Warehouse added " + name);
+    public void addProduct(Products prod) {
+        if (productsList.containsKey(prod.getId())) {
+            System.out.println("Product ID already exists");
         } else {
-            System.out.println("Warehouse already exists");
+            productsList.put(prod.getId(), prod);
+            System.out.println( "Product added: " + prod.getName());
         }
     }
 
-    public void addItem(String warehouseName, Item item) {
-        Warehouse warehouse = warehouses.get(warehouseName);
-        if (warehouse != null) {
-            warehouse.addItem(item);
-        } else {
-            System.out.println("Warehouse not found");
-        }
+    public Products getProduct(int id) {
+        return productsList.get(id);
     }
 
-    public void removeItem(String warehouseName, String itemName) {
-        Warehouse warehouse = warehouses.get(warehouseName);
-        if (warehouse != null) {
-            warehouse.removeItem(itemName);
-        } else {
-            System.out.println("Warehouse not found");
-        }
-    }
-
-    public void updateItemQuantity(String warehouseName, String itemName, int newQuantity) {
-        Warehouse warehouse = warehouses.get(warehouseName);
-        if (warehouse != null) {
-            warehouse.updateItemQuantity(itemName, newQuantity);
-        } else {
-            System.out.println("Warehouse not found");
-        }
-    }
-
-    public void displayWarehouseItems(String warehouseName) {
-        Warehouse warehouse = warehouses.get(warehouseName);
-        if (warehouse != null) {
-            warehouse.displayItems();
-        } else {
-            System.out.println("Warehouse not found");
-        }
-    }
-
-    public void displayAllWarehouses() {
-        if (warehouses.isEmpty()) {
-            System.out.println("No warehouses found");
-        } else {
-            System.out.println("Warehouses in system");
-            for (String name : warehouses.keySet()) {
-                System.out.println(" - " + name);
-            }
+    public void displayAll() {
+        System.out.println("\nInventory List");
+        for (Products prod : productsList.values()) {
+            System.out.println(prod);
         }
     }
 }
+
