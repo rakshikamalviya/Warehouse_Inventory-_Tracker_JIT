533a2b2cc4b9c361cbaf3b89a830b4a397f5306c
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-30 18:50:04 +0530

    Fixed product class Errors

--- src/products.java
+++ src/products.java
@@ -1,8 +1,10 @@
+
 public class Products {
     private int id;
     private String name;
     private int quantity;
     private int reorderThreshold;
+
     public Products(int id, String name, int quantity, int reorderThreshold) {
         this.id = id;
         this.name = name;
@@ -10,44 +12,21 @@ public class Products {
         this.reorderThreshold = reorderThreshold;
     }
 
-    public int getId() {
-        return id;
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getQuantity() { return quantity; }
+    public int getReorderThreshold() { return reorderThreshold; }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    public String getName() {
-        return name;
+    public boolean isLowStock() {
+        return quantity < reorderThreshold;
     }
 
-    public int getQuantity() {
-        return quantity;
+    @Override
+    public String toString() {
+        return "Products[ID=" + id + ", Name=" + name + ", Quantity=" + quantity + ", Threshold=" + reorderThreshold + "]";
     }
-
-    public int getReorderThreshold() {
-        return reorderThreshold;
-    }
-
-public synchronized void increaseQuantity(int amount) {
-    if (amount < 0) {
-        throw new IllegalArgumentException("Amount must be non-negative");
-    }
-    this.quantity += amount;
-}
-
-public synchronized void decreaseQuantity(int amount) {
-    if (amount < 0) {
-        throw new IllegalArgumentException("Amount must be non-negative");
-    }
-    if (amount > this.quantity) {
-        throw new IllegalArgumentException("Insufficient stock available");
-    }
-    this.quantity -= amount;
-}
-
-@Override
-public String toString() {
-    return String.format(
-        "Products[id=%d, name=%s, quantity=%d, threshold=%d]",id, name, quantity, reorderThreshold
-    );
-}
-
 }
