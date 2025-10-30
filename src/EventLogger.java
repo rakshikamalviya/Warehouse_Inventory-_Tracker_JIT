6428b1e63c63e2f03f57f5590e322cdc760943bd
rakshika malviya <rakshikamalviya3@gmail.com>
2025-10-30 19:00:44 +0530

    Fixed EventLogger class

--- src/EventLogger.java
+++ src/EventLogger.java
@@ -1,23 +1,10 @@
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class EventLogger {
-
-    private static final String LOG_FILE = "event_log.txt"; 
-
-    public static void log(String message) {
-        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        String logMessage = "[" + timestamp + "] " + message;
-
-    
-        System.out.println(logMessage);
-
-        try (FileWriter writer = new FileWriter(LOG_FILE, true)) {
-            writer.write(logMessage + "\n");
-        } catch (IOException e) {
-            System.err.println("Error while writing to log file " + e.getMessage());
-        }
+    public void log(String message) {
+        LocalDateTime now = LocalDateTime.now();
+        String time = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println("[LOG " + time + "] " + message);
     }
 }
